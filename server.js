40e6e1232cd18a917b1d506eb753796a70bdc79c
sharpevb <victoriabsharpe@gmail.com>
2019-09-25 23:13:43 -0400

    app scraping info and populating browser

--- server.js
+++ server.js
@@ -1,19 +1,95 @@
+// Dependencies
 var express = require("express");
+var mongojs = require("mongojs");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
 let exphbs = require("express-handlebars");
+
 var db = require("./models");
+
 var PORT = 3000;
+
 var app = express();
 
-app.use(express.urlencoded({ extended: true }));
+// Database configuration
+var databaseUrl = "mongoHeadlines";
+var collections = ["Article"];
+
+// Logging requests
+app.use(logger("dev"));
+
+// JSON
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Use assets in public folder
 app.use(express.static("public"));
 
+
+// Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
+// Routes //
+app.get('/', function (req, res, next) {
+    res.render('index', {layout: false});
+});
+
+app.get("/scrape", function (req, res) {
+    axios.get("https://old.reddit.com/r/UpliftingNews/").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $("p.title").each(function (i, element) {
+
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+            console.log(result);
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+
+    });
+
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+
+
 
 
 
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
-  });
+});
+
+module.exports = app;
